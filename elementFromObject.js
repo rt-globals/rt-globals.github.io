7a813c23b171070a0f2a8a8ad69494b801fdd1c1
Danny Engelman <danny@engelman.nl>
2022-12-04 11:42:59 +0100

    enhanced with append,prepend,cssText

--- elementFromObject.js
+++ elementFromObject.js
@@ -21,24 +21,40 @@
 // });
 
 window.elementFromObject = ({
-  tag = "div", // tagname OR existing Node
-  classes = [], // all added class names
-  attrs = {}, // all added attributes
-  cssText = false, // tag.style.cssText string!
-  styles = {}, // all added styles
-  prepend = [], // prepended Children
-  append = [], // appended Children
-  ...props // all remaining properties, event handlers etc.
+  tag = "div", //     string OR existing DOM element
+  innerHTML = ``, //  capture now so append can append childelements after it
+  cssText = ``, //    String of CSS REPLACES existing styles
+  classes = [], //    array of class names
+  styles = {}, //     object with css properties
+  attrs = {}, //      {name:value}
+  append = [], //     Array of DOM elements to append
+  prepend = [], //    Array of DOM elements to prepend
+  ...props //         any other properties AND EventHandlers
 } = {}) => {
-  tag = typeof tag == "string" ? document.createElement(tag) : tag;
-  //this.setAttributes(attrs, el);
-  Object.keys(attrs).map((key) => tag.setAttribute(key, attrs[key]));
+  tag = typeof tag == "string" ? document.createElement(tag) : tag; //   create tag OR use tag
   cssText && (tag.style.cssText = cssText);
-  Object.keys(styles).map((key) => (tag.style[key] = styles[key]));
-  tag.classList.add(...classes);
+  Object.keys(attrs).map((key) => tag.setAttribute(key, attrs[key])); // set attributes
+  Object.keys(styles).map((key) => (tag.style[key] = styles[key])); //   set styles
+  tag.classList.add(...classes); //
   tag.prepend(...prepend.flat());
+  innerHTML && (tag.innerHTML = innerHTML);
   tag.append(...append.flat());
-  Object.assign(tag, props);
-  return tag;
+  return Object.assign(tag, props);
 };
 
+  // TODO: add support for innerHTML Template Literal String, replacing this. properties
+  // let parseStringLiteral = (str, v = {}) => {
+  //   console.log("parseStringLiteral", { str: [str] }, v);
+  //   try {
+  //     return
+  //       new Function("v", "return((" + Object.keys(v).join(",") + ")=>`" + str + "`)(...Object.values(v))")(v) || "";
+  //   } catch (e) {
+  //     console.error(
+  //       "parseStringLiteral",
+  //       v,
+  //       str
+  //     );
+  //     //! DO not return ""; this will list the error for every frame
+  //     console.error(new Error().stack);
+  //   }
+  // };
